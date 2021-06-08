f8014b671bcd5cd32dcc378eea88dd5cd753e7ff
Welber <welber.fa@gmail.com>
2021-06-08 20:45:07 -0300

    Create handle_category_data.rb

---
+++ handle_category_data.rb
@@ -0,0 +1,19 @@
+# Retorna as chaves
+bucket.keys
+# Retorna os valores
+bucket.values
+
+# Armazena valores
+a = bucket.values 
+
+category_data = params.values
+
+# conta elementos distintos
+a.tally
+
+ # Retorna quantidade de elementos
+ a.tally["Pode Melhorar"] 
+
+ a.tally["Excelente"]
+ 
+ a.tally["Mais Atenção"]
